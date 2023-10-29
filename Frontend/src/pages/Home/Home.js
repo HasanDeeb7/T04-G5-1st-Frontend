0c04aaa228f42b96f6cc8f2b3afc978a528d9e4d
Hasan Deeb <sektories7@gmail.com>
2023-10-29 14:29:29 +0200

    added margin to all pages

--- Frontend/src/pages/Home/Home.js
+++ Frontend/src/pages/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ServicesSection from '../../layouts/HomepageServices/index'
 import GymPlans from '../../components/Plans/GymPlans'
-import './home.css'
 
 function Home() {
   return (
